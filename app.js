840db703e235639494cd0e9bd279e95cadff9569
gi_ryun_Lim <98068075+giirrr@users.noreply.github.com>
2023-08-11 19:34:39 +0900

    Css in JS

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
-  h1.style.color = "tomato";
-}
-function handleMouseEnter() {
-  h1.innerText = "mouse is here!";
-}
-function handleMouseLeave() {
-  h1.innerText = "mouse is gone!";
-}
-
-function handleResize() {
-  document.body.style.backgroundColor = "yellow";
-}
-function handleWindowCopy() {
-  alert("copier!");
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-function handleWindowOnline() {
-  alert("ALL GOOOD");
-}
-
-h1.onclick = handleTitleClick;
-h1.onmouseenter = handleMouseEnter;
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
