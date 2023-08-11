e4ea96799105b87fa9be8aa2ddf1432edb2a41e7
gi_ryun_Lim <98068075+giirrr@users.noreply.github.com>
2023-08-11 20:30:09 +0900

    classList.contain, remove, add & toggle

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
-  const clickedClass = "clicked sexy-font";
-  if (h1.className === clickedClass) {
-    h1.className = "";
-  } else {
-    h1.className = clickedClass;
-  }
+  const clickedClass = "clicked";
+  h1.classList.toggle(clickedClass);
+
+  //******* classList.contain remove add ******
+  //   if (h1.classList.contains(clickedClass)) {
+  //     h1.classList.remove(clickedClass);
+  //   } else {
+  //     h1.classList.add(clickedClass);
+  //   }
 }
 h1.addEventListener("click", handleTitleClick);
