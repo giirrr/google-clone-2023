538b6ad15c51a12cb7d87a8f0169fbfa61ec1bb7
gi_ryun_Lim <98068075+giirrr@users.noreply.github.com>
2023-08-11 17:36:24 +0900

    event 2

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const title = document.querySelector(".hello:first-child h1");
 
-function handTitleClick() {
+function handleTitleClick() {
   title.style.color = "tomato";
 }
-title.addEventListener("click", handTitleClick);
+function handleMouseEnter() {
+  title.innerText = "mouse is here!";
+}
+function handleMouseLeave() {
+  title.innerText = "mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
