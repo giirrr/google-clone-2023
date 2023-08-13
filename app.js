c55b90abe661c0a1e425e83f88299b1fcdfbb2b8
gi_ryun_Lim <98068075+giirrr@users.noreply.github.com>
2023-08-13 20:52:35 +0900

    recap

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+  paintGreetings();
 }
 
 function paintGreetings(username) {
+  //   const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
