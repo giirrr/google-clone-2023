dc727035a2b33e611c3668bffe7da9d4ad28b1a1
gi_ryun_Lim <98068075+giirrr@users.noreply.github.com>
2023-08-13 18:55:47 +0900

    argument&event

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginBwutton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
-  console.log(username);
-  //   if (username === "") {
-  //     alert("Please write your name");
-  //   } else if (username.length > 15) {
-  //     alert("Your name is too long.  ");
-  //   }
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
-loginBwutton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
