7c95521f59f64f024016c6106809a8e870ea796c
gi_ryun_Lim <98068075+giirrr@users.noreply.github.com>
2023-08-17 04:00:54 +0900

    delete todo

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-List");
 
-function paintToDo(newTodoapple) {
-  const potato = document.createElement("li");
-  const span = document.createElement("span");
-  potato.appendChild(span);
-  span.innerText = newTodoapple;
-  toDoList.appendChild(potato);
+function deleteToDo(event) {
+  console.dir(event.target.parentElement.innerText);
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newToA) {
+  const li1 = document.createElement("li");
+  const span1 = document.createElement("span");
+  span1.innerText = newToA;
+  const button1 = document.createElement("button");
+  button1.innerText = "❌";
+  button1.addEventListener("click", deleteToDo);
+  li1.appendChild(span1);
+  li1.appendChild(button1);
+  toDoList.appendChild(li1);
 }
 
 function handleToDoSubmit(event) {
