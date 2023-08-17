685dd857d6abfbafc2a0a4abee8eda54ea55e5f6
gi_ryun_Lim <98068075+giirrr@users.noreply.github.com>
2023-08-17 17:16:12 +0900

    geolocation using API

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "064930866b1cfeaf6667df2da8141d9a";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log("You live in", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  //   Js call url
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main}/${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
